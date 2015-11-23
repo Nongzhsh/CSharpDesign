b012d2ce3f3108e356f0c1c16969c5792a252baa
晨星宇 <1025395601@qq.com>
2015-11-23 23:29:00 +0800

    添加完善享元模式

--- 享元模式/Program.cs
+++ 享元模式/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace 享元模式
 {
@@ -10,6 +7,54 @@ namespace 享元模式
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("小明和女神创建一个小团队");
+            TeamFactory fa = new TeamFactory();
+            Team teamM = fa.GetTeam("小明");
+            teamM.Introduce("男");
+            Team nvS = fa.GetTeam("女神");
+            nvS.Introduce("女");
+            Team teamG = fa.GetTeam("小刚");
+            teamG.Introduce("男");
+
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Team
+    {
+        public abstract void Introduce(string strSex);
+    }
+
+    public class SomeMTeam : Team
+    {
+        private string name;
+        public SomeMTeam(string strName) {
+            this.name = strName;
+        }
+        public override void Introduce(string strSex)
+        {
+            Console.WriteLine("姓名，" + name + "。性别，" + strSex);
+        }
+    }
+
+    public class TeamFactory
+    {
+        public Dictionary<string, Team> teamList = new Dictionary<string, Team>();
+        public TeamFactory() {
+            teamList.Add("小明", new SomeMTeam("小明"));
+            teamList.Add("女神", new SomeMTeam("女神"));
+        }
+        public Team GetTeam(string key)
+        {
+            Team team = null;
+            if (teamList.ContainsKey(key))
+                return teamList[key] as Team;
+            else
+            {
+                Console.WriteLine("新人加入");
+                team = new SomeMTeam(key);
+                return team;
+            }
         }
     }
 }
