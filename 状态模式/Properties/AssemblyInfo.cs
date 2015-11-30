dc54b07fb35b7e12ac708e9576fb8046e3dd09ef
晨星宇 <1025395601@qq.com>
2015-11-30 21:04:38 +0800

    添加所有23的种设计模式

--- 状态模式/Properties/AssemblyInfo.cs
+++ 状态模式/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的一般信息由以下
@@ -22,7 +21,7 @@ using System.Runtime.InteropServices;
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("5194914a-ec37-4a17-aa28-d35385d4dbf6")]
 
-// 程序集的版本信息由下列四个值组成: 
+// 程序集的版本信息由下列四个值组成:
 //
 //      主版本
 //      次版本
